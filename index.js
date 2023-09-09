602493700b572b825ec0c25bcc298d01aa2c2171
AndiMSF <andimuhammadsatriafadhil77@gmail.com>
2023-09-09 20:12:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+const ejs = require("ejs")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const flash = require("connect-flash")
+
+// Import verifyToken
+const verifyToken = require("./middleware/auth.js")
+
+
+// Import Routes
+const authRoutes = require("./routes/auth.js")
+// const homeRoutes = require("./routes/home.js")
+// const composeRoutes = require("./routes/compose.js")
+// const itemRoutes = require("./routes/item.js")
+// const deleteRoutes = require("./routes/delete.js")
+// const editRoutes = require("./routes/edit.js")
+
+// Configuration
+const app = express()
+dotenv.config()
+app.use(cookieParser())
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+ "/public"));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 60000 },
+    saveUninitialized: false,
+    resave: false
+}))
+app.use(flash())
+
+
+// MongoDB Setup
+const connectDatabase = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@cluster0.vqcsdra.mongodb.net/InventoryKu',
+            {
+                useNewUrlParser:true,
+                useUnifiedTopology:true
+            }
+        )
+        console.log("Database Connected "+conn)
+    } catch (err) {
+        console.log(err)
+    }  
+}
+const PORT = 8080
+connectDatabase().then(() => {
+    app.listen(PORT, function() {
+        console.log("Server berjalan di port "+PORT);
+      })
+})
+
+app.get("/", (req, res) => {
+    if(req.cookies.jwt){
+        res.redirect("/userhome")
+    } else {
+        res.render("home")
+    }
+})
+app.get("/userhome", verifyToken, (req, res) => {
+    res.render("homeLoggedUser")
+})
+app.get("/logout", (req, res) => {
+   res.cookie("jwt", "", { maxAge: 1 })
+   res.redirect("/")
+})
+
+
+app.use("/auth", authRoutes)
+// app.use("/home", homeRoutes)
+// app.use("/items", itemRoutes)
+// app.use("/compose", composeRoutes)
+// app.use("/delete", deleteRoutes)
+// app.use("/edit", editRoutes)
